d8f2f870ca0de235ffd50f36c51e6d1777bc366c
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-12-14 23:18:49 -0500

    Creates models and transaction model.
    This commit creates the models folder which will hold all the
    models for the project. This commit also creates the
    transaction model which is the main model that will be used
    to store the transaction data from the csv files.

---
+++ src/models/transaction.js
@@ -0,0 +1,35 @@
+/**
+ * This is the transaction model.
+ */
+
+// Imports.
+const mongoose = require('mongoose');
+
+// Transaction schema.
+const transactionSchema = new mongoose.Schema({
+    date: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+// Create the Transaction model using the schema.
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+// Export the model.
+module.exports = Transaction;
