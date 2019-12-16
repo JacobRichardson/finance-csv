dec06410e0f53d4a2827254ed024956a927f14c3
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-12-16 01:37:33 -0500

    Whitespace changes src/models/transaction.
    This commit updates the transaction model to have the correct
    whitespace formatting.

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -7,25 +7,25 @@ const mongoose = require('mongoose');
 
 // Transaction schema.
 const transactionSchema = new mongoose.Schema({
-    date: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    category: {
-        type: String
-    },
-    amount: {
-        type: Number,
-        required: true
-    },
-    created_at: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
+	date: {
+		type: String
+		, required: true
+	}
+	, description: {
+		type: String
+	}
+	, category: {
+		type: String
+	}
+	, amount: {
+		type: Number
+		, required: true
+	}
+	, created_at: {
+		type: Date
+		, required: true
+		, default: Date.now
+	}
 });
 
 // Create the Transaction model using the schema.
