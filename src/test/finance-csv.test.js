d08cdac47a8e841fd272c3dde8941ee1ccbe19f5
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-12-21 12:25:32 -0500

    Updating src/test/finance-csv - the wrong file was being tested.

--- src/test/finance-csv.test.js
+++ src/test/finance-csv.test.js
@@ -11,12 +11,13 @@ test('finance csv class complies', t => {
 	try {
 
 		// Require in the module.
-		require('../lib/toJson');
+		require('../app/finance-csv');
 
 		// Pass the test because there is no error requiring in the project.
 		t.pass('No error requiring in finance csv class.');
 
-	} catch (e) {
+	}
+	catch (e) {
 
 		// Fail the test with the error.
 		t.fail(e);
